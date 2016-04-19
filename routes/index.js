614a0592f59954c942229440cf650232d08aaf36
Kim Hyojong <justinhyojong@gmail.com>
2016-04-19 08:44:03 +0100

    ..

--- routes/index.js
+++ routes/index.js
@@ -176,10 +176,6 @@ router.get('/userStats/:username/:attribute',function(req,res){
 		var data = [];
 
 		query
-		.on('error',function(err){
-			done();
-			return res.json(500).json({success:false,data:err});
-		})
 		.on('row',function(row){
 			data.push(row);
 		})
